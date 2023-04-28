62e696eeac451cc64efa97de8a4dad54fd579b08
nigi1992 <nicolas.waser@icloud.com>
2023-04-28 19:16:50 +0200

    - worked on ggplot image and saved it in folder - tried some more to overlay map with spatial data, didn't work

--- Spatial analysis and Data Vizualization.R
+++ Spatial analysis and Data Vizualization.R
@@ -8,6 +8,8 @@ install.packages("sp")
 library(sp)
 library(ggplot2)
 
+install.packages("readxl")
+library(readxl)
 # import excel file
 df <- read_excel("halteplaetze-jenische_sinti_roma_2056.xlsx")
 
@@ -16,7 +18,11 @@ df <- read_excel("halteplaetze-jenische_sinti_roma_2056.xlsx")
 data_sf <- st_as_sf(data_clean, coords = c("E-Koordinate", "N-Koordinate"), crs = "+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 
 # plot the spatial data
-ggplot(data_sf) + geom_sf()
+ggplot(data_sf) + geom_sf() + labs(title = "Geolocations of Caravan Spots for Fahrende in Switzerland", x="Longitude", y="Latitude")
+
+library(here)
+save(ggplot, file = here("data", "ggplot1.RData")) #saving ggplot in R format
+ggsave(file = here("images", "Image1.png")) #saving as image
 
 # Underlie a map of Switzerland under this data
 install.packages("ggmap")
@@ -27,7 +33,7 @@ data_sf <- st_as_sf(data_clean, coords = c("E-Koordinate", "N-Koordinate"), crs 
 # Get the bounding box for Switzerland
 switzerland_bbox <- c(left = 5.96, bottom = 45.82, right = 10.49, top = 47.81)
 
-# dang it! Google now requires an API Key, might have to use an API after all
+# dang it! Google now requires an API Key, will have to use an API after all
 ggmap::register_google()
 browseURL(url = "https://mapsplatform.google.com/")
 api_key <- rstudioapi::askForPassword()
@@ -38,8 +44,10 @@ register_google(api_key)
 switzerland_map <- get_map(location = switzerland_bbox, maptype = "roadmap", zoom = 7)
 
 # Plot the map and the spatial data
-ggmap(switzerland_map) + geom_sf(data = data_clean)
-  
+ggmap(switzerland_map) + geom_sf() #nope
+ggmap(switzerland_map) + geom_sf(data = data_sf) #nada
+ggmap(switzerland_map) + geom_sf(data = data_clean, aes(x = "E-Koordinate", y = "N-Koordinate")) #niente
+# did not work out. Cant overlay the data over the map. Moving on.
 
 
 # Conduct spatial analysis

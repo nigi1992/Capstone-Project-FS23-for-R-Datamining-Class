429b6640e7ad814c84e034569a420d4c941c125e
nigi1992 <nicolas.waser@icloud.com>
2023-05-05 23:38:13 +0200

    - deleted a bunch of files for structure

--- Notes.R
+++
@@ -1,6 +0,0 @@
-## Notes for Capstone Project
-
-# See Admin website for some info on the Fahrende and give a short description of what they are about
-# also what this data is about and what you want to do with it
-
-# this project data and it's generated data, graphs and knowledge might be used as a starting point to work on a paper on the Fahrende
